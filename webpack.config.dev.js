75009434ebf525b420cf6c896daaf99c1264cf41
Ivor Lewis <101133399+00ives@users.noreply.github.com>
2023-08-28 19:02:11 -0600

    environment build complete

---
+++ webpack.config.dev.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack'); //import webpack itself
+const path = require('path'); // allows us to work with paths
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //
+
+process.env.NODE_ENV = 'development'; // declares node environment important for babel plugin to know we are working with node
+module.exports = {
+  mode: 'development',
+  target: 'web',
+  devtool: 'cheap-module-source-map',
+  entry: './src/index',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    stats: 'minimal',
+    overlay: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    https: false,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      favicon: 'src/favicon.ico',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /(\.css)$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
